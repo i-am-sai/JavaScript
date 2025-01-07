08ff4e48b9571c5e804cfa5fdb30ac8944ddcce1
unknown <saiswaroopkdevarmane@gmail.com>
2025-01-07 23:24:53 +0530

    third

---
+++ httpMethods.js
@@ -0,0 +1,14 @@
+// HTTP : is designed to enable comminucation b/w client and servers
+// client sends a HTTP request and server responds and the response contains status information about the request and may also contain the requested content.
+
+// HTTP Methods : Get, Post, Put, Patch, Delete, Head, Options, Connect, Trace.
+// 1. GET  : is used to request data from a aspecified resource\
+// 2. Post : is used to send data to a server to create/update a resource. add 
+// 3. Put : is used to send data to server to create/update a resource. replace a resource
+// 4. Head : is almost identical to Get, but without the response body.
+// 5. Delete : method deltes the specified resource.
+// 6. Patch : is used to apply partial modifications to a resource.
+// 7. Trace : is used to perform a message loop-back test that test the path for the target resource (useful for debugging purpose).
+// 8. Connect : is used to start a 2 way communications a tunnel with the requested resource.
+// 9. Options : describes the communication options for the target resource.
+
